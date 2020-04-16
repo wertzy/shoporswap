c4fb1ebb9ba8b88bf00a32fc2ef157755b4201de
IoanDascalu <idascalu@ithaca.edu>
2020-04-16 13:08:19 -0400

    Created new branch

---
+++ src/test/java/SearchProductTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+    public class SearchProductTest {
+        @Test
+        void searchTest() {
+
+        }
+}
