aaddf0d9257e54aa6dffb855f477816260216a7e
Desmond <desmond_1818@hotmail.com>
2020-04-20 19:55:43 -0400

    removed shopper,swapper, and seller classes
    these three classes served no purpose

--- src/main/java/Report.java
+++ src/main/java/Report.java
@@ -1,14 +1,14 @@
 import java.util.*;
-/*
-public class Report(String ID, String Comment){
-		String Comment,ID;
-public Report(String ID,String Comment){
+
+/* public class Report (String ID, String Comment){
+		public String Comment,ID;
+public void Report(String ID, String Comment){
 		this.ID=ID;
 		this.Comment=Comment;
 		}
-public persist(String adminact){
-		adminact.sendEmail(ID,Comment);
+public void persist(String adminact){
+		adminact.sendEmail(ID, Comment);
 
 		}
-}
-*/
+		} */
+
