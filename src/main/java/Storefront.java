e66a2a753a07d9dcbff36c33fde82ed7381983b9
Justin Moczynski <jmoczynski@ithaca.edu>
2020-04-29 15:31:21 -0400

    added abstract class structure: Storefront class

---
+++ src/main/java/Storefront.java
@@ -0,0 +1,37 @@
+
+public abstract class Storefront {
+
+    private String storefrontName;
+    private User storefrontOwner;
+
+    /**
+     * Accessor method for the name of the Storefront
+     * @return the name of the Storefront
+     */
+    public final String getStorefrontOwnerName(){
+        return "";
+    }
+
+    /**
+     * Accessor method for the owner of the Storefront
+     * @return the owner of the Storefront
+     */
+    public final User getStorefrontOwner(){
+        return null;
+    }
+
+    /**
+     * Mutator method for the name of the Storefront
+     * @param nameIn the name to set
+     */
+    public final void setStorefrontName(String nameIn){
+    }
+
+    /**
+     * Mutator method for the name of the Storefront
+     * @param ownerIn the owner to set
+     */
+    public final void setStorefrontOwner(User ownerIn){
+    }
+
+}
