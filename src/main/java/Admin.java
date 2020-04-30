d47bcc1d868ea3a34fc4f1125ade256287c192d8
Justin Moczynski <jmoczynski@ithaca.edu>
2020-04-30 19:46:55 -0400

    updated class structures: Admin class

--- src/main/java/Admin.java
+++ src/main/java/Admin.java
@@ -1,2 +1,30 @@
 public class Admin extends Account {
+
+    /**
+     * Default constructor for Admin Account
+     */
+    public Admin(){
+
+    }
+
+    /**
+     * Constructor for Admin Account
+     * @param nameIn the name of the Admin Account
+     * @param passwordIn the password of the Admin Account
+     * @throws IllegalArgumentException if nameIn is invalid
+     * @throws IllegalArgumentException if passwordIn is invalid
+     */
+    public Admin(String nameIn, String passwordIn){
+
+    }
+
+    /**
+     * Method to freeze accounts
+     * @param clientIn the Client to freeze
+     * @return the Client frozen
+     */
+    public Client freezeAccount(Client clientIn){
+        return null;
+    }
+
 }
