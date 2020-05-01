de2f891d2cfe9990aca12891cbfdbf6a6bc990bd
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-01 10:51:21 -0400

    created class structure: StorefrontFactory class

---
+++ src/main/java/StorefrontFactory.java
@@ -0,0 +1,12 @@
+public class StorefrontFactory {
+
+    /**
+     * Creates a new Storefront based on the desired Storefront
+     * @param storefrontType the type of Storefront as a string
+     * @return a new Storefront object
+     * @throws IllegalArgumentException if the Storefront type is invalid (valid types are "sell" or "swap")
+     */
+    public Storefront getSellStorefront(String storefrontType){
+        return null;
+    }
+}
