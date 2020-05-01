9e9350584f96fbad9d542ef11c3ac256c107c270
IoanDascalu <idascalu@ithaca.edu>
2020-05-01 12:28:55 -0400

    Minor changes to HelloFX test fx file.

--- src/main/java/HelloFX.java
+++ src/main/java/HelloFX.java
@@ -1,5 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -11,6 +12,7 @@ public class HelloFX extends Application {
         String javaVersion = System.getProperty("java.version");
         String javafxVersion = System.getProperty("javafx.version");
         Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        //Button q = new Button()
         Scene scene = new Scene(new StackPane(l), 640, 480);
         stage.setScene(scene);
         stage.show();
