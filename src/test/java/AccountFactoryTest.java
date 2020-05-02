481f90002066690fc9ed70c434692130a3572cb7
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-02 07:24:02 -0400

    added automated test structure: AccountFactory

---
+++ src/test/java/AccountFactoryTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class AccountFactoryTest {
+
+    /**
+     * Automated tests for AccountFactory.getAccount method
+     */
+    @Test
+    void getAccountTest(){
+
+    }
+
+}
