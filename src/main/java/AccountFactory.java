145232adab3355982e46f4202108a2a72131e542
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-02 07:18:19 -0400

    added class structure: AccountFactory

---
+++ src/main/java/AccountFactory.java
@@ -0,0 +1,20 @@
+public class AccountFactory {
+
+    /**
+     * Default constructor for AccountFactory class
+     */
+    public AccountFactory(){
+
+    }
+
+    /**
+     * Creates an Account based on input about what type
+     * @param typeIn the type of Account
+     * @return the Account (Client or Admin)
+     * @throws IllegalArgumentException if typeIn is invalid
+     */
+    public Account getAccount(String typeIn){
+        return null;
+    }
+
+}
