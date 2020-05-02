a5f2fdf668ba599847c72631188ddd5ff3bf4e16
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-02 07:39:06 -0400

    added automated test implementation: AccountFactory

--- src/main/java/AccountFactory.java
+++ src/main/java/AccountFactory.java
@@ -11,7 +11,7 @@ public class AccountFactory {
      * Creates an Account based on input about what type
      * @param typeIn the type of Account
      * @return the Account (Client or Admin)
-     * @throws IllegalArgumentException if typeIn is invalid
+     * @throws IllegalArgumentException if typeIn is invalid (not "client" or "admin")
      */
     public Account getAccount(String typeIn){
         return null;
