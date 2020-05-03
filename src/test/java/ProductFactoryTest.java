755f55e91cbb0154b3ba37e75837d8099cc70b16
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-03 10:06:35 -0400

    added automated test structure: ProductFactory

---
+++ src/test/java/ProductFactoryTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class ProductFactoryTest {
+
+    /**
+     * Automated tests for ProductFactory.getProduct method
+     */
+    @Test
+    void getProductTest(){
+
+    }
+    
+}
