d86c1c728ab244514fc20fe7e4654f47b4469417
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-03 09:59:35 -0400

    added class structure: ProductFactory

---
+++ src/main/java/ProductFactory.java
@@ -0,0 +1,20 @@
+public class ProductFactory {
+
+    /**
+     * Default constructor for ProductFactory
+     */
+    public ProductFactory(){
+
+    }
+
+    /**
+     * Creates a new AbstractProduct
+     * @param typeIn the type of Product to create
+     * @return a SellProduct or SwapProduct
+     * @throws IllegalArgumentException if typeIn is invalid (if typeIn is neither "sell" nor "swap")
+     */
+    public AbstractProduct getProduct(String typeIn){
+        return null;
+    }
+
+}
