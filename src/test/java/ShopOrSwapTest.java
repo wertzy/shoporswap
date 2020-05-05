31eeaf137415af580b060f03113e8d01bdeebf70
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-05 15:01:31 -0400

    added automated test structure: ShopOrSwap class

---
+++ src/test/java/ShopOrSwapTest.java
@@ -0,0 +1,102 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShopOrSwapTest {
+
+    /**
+     * Automated tests for ShopOrSwap constructor methods
+     */
+    void constructorsTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap mutator methods
+     */
+    void mutatorsTests(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.addAccount method
+     */
+    void addAccountTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.findAccount method
+     */
+    void findAccountTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.removeAccount method
+     */
+    void removeAccountTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.addProduct method
+     */
+    void addProductTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.findProduct method
+     */
+    void findProductTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.removeProduct method
+     */
+    void removeProductTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.addStorefront method
+     */
+    void addStorefrontTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.findStorefront method
+     */
+    void findStorefrontTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.removeStorefront method
+     */
+    void removeStorefrontTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.buyProduct method
+     */
+    void buyProductTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.swapProducts method
+     */
+    void swapProductsTest(){
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for ShopOrSwap.sendMessage method
+     */
+    void sendMessageTest(){
+        assertTrue(false);
+    }
+}
