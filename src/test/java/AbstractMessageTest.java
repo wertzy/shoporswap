24cc0c0cf02281e369d448269dc36f703c3917a5
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-04 23:04:44 -0400

    added automated test structure: AbstractMessage class

---
+++ src/test/java/AbstractMessageTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AbstractMessageTest {
+
+    /**
+     * Automated tests for constructor methods of AbstractMessage
+     */
+    @Test
+    void constructorsTests(){
+        //TODO implement automated tests
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for mutator methods of AbstractMessage
+     */
+    @Test
+    void mutatorsTests(){
+        //TODO implement automated tests
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for AbstractMessage.isValidMessageSender method
+     */
+    @Test
+    void isValidMessageSenderTest(){
+        //TODO implement automated tests
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for AbstractMessage.isValidMessageSubject method
+     */
+    @Test
+    void isValidMessageSubjectTest(){
+        //TODO implement automated tests
+        assertTrue(false);
+    }
+
+    /**
+     * Automated tests for AbstractMessage.isValidMessageContent method
+     */
+    @Test
+    void isValidMessageContentTest(){
+        //TODO implement automated tests
+        assertTrue(false);
+    }
+
+}
