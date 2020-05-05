e11a18e734e28c94cbb9fad2b4580e084ce88d2c
IoanDascalu <idascalu@ithaca.edu>
2020-05-04 23:35:24 -0400

    Added module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module shoporswap {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+
+    opens gui;
+}
