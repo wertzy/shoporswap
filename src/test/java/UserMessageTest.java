0a9a12ba3275154e9f2b82a01a202dd41c7419ad
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-04 22:48:30 -0400

    added automated test structure: UserMessage class

---
+++ src/test/java/UserMessageTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+public class UserMessageTest {
+
+    /**
+     * Automated tests for constructor methods of UserMessage
+     */
+    @Test
+    void constructorsTests(){
+        //TODO implement automated tests
+    }
+
+}
