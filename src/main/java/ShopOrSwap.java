d01fe4e35b05bdac4cce0c8b5cdab80299f01c46
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-05 14:48:18 -0400

    updated class structure: ShopOrSwap class

--- src/main/java/ShopOrSwap.java
+++ src/main/java/ShopOrSwap.java
@@ -7,34 +7,121 @@ public class ShopOrSwap {
     private AccountFactory accountFactory;
     private StorefrontFactory storefrontFactory;
     private AbstractProductFactory productFactory;
+
     private Map<String, Account> accountCollection;
-    private String dataFileName;
 
     /**
-     * Default constructor for ShopOrSwap object
+     * Default constructor for the ShopOrSwap system
      */
     public ShopOrSwap(){}
 
     /**
-     * Constructor for ShopOrSwap object
-     * @param dataFileNameIn the name of the JSON file to load data from
-     * @throws IOException if dataFileIn is not found
-     */
-    public ShopOrSwap(String dataFileNameIn){}
-
-    /**
-     * Accessor method for the data file property of the ShopOrSwap object
+     * Adds an Account to the ShopOrSwap system
+     * @param accountIn
      * @return
      */
-    public String getDataFileName(){
-        return "";
+    public Account addAccount(Account accountIn){
+        return null;
     }
 
     /**
-     * Mutator method for the data file property of the ShopOrSwap object
-     * @param dataFileNameIn the name of the JSON file to load data from
-     * @throws IOException if dataFileNameIn is not found
+     * Finds an Account in the ShopOrSwap system
+     * @param accountIn
+     * @return
      */
-    public void setDataFileName(String dataFileNameIn){}
+    public Account findAccount(Account accountIn){
+        return null;
+    }
+
+    /**
+     * Removes an Account from the ShopOrSwap system
+     * @param accountIn
+     * @return
+     */
+    public Account removeAccount(Account accountIn){
+        return null;
+    }
+
+    /**
+     * Adds a product to the ShopOrSwap system
+     * @param productIn
+     * @return
+     */
+    public AbstractProduct addProduct(AbstractProduct productIn){
+        return null;
+    }
+
+    /**
+     * Finds a product to the ShopOrSwap system
+     * @param productIn
+     * @return
+     */
+    public AbstractProduct findProduct(AbstractProduct productIn){
+        return null;
+    }
+
+    /**
+     * Removes a product from the ShopOrSwap system
+     * @param productIn
+     * @return
+     */
+    public AbstractProduct removeProduct(AbstractProduct productIn){
+        return null;
+    }
+
+    /**
+     * Adds a Storefront to the ShopOrSwap system
+     * @param storefrontIn
+     * @return
+     */
+    public Storefront addStorefront(Storefront storefrontIn){
+        return null;
+    }
+
+    /**
+     * Finds a Storefront in the ShopOrSwap system
+     * @param storefrontIn
+     * @return
+     */
+    public Storefront findStorefront(Storefront storefrontIn){
+        return null;
+    }
+
+    /**
+     * Removes a Storefront from the ShopOrSwap system
+     * @param storefrontIn
+     * @return
+     */
+    public Storefront removeStorefront(Storefront storefrontIn){
+        return null;
+    }
+
+    /**
+     * System process for buying products
+     * @param productToBuy
+     * @param consumerIn
+     */
+    public void buyProduct(AbstractProduct productToBuy, Client consumerIn){}
+
+    /**
+     * System process for swapping products
+     * @param product1
+     * @param product2
+     */
+    public void swapProducts(AbstractProduct product1, AbstractProduct product2){}
+
+    /**
+     * System process for message communications
+     * @param messageIn
+     */
+    public void sendMessage(AbstractMessage messageIn){}
+
+    public Map<String, Account> getAccountCollection(){
+        return null;
+    }
+
+    public void setAccountCollection(Map<String, Account> accountCollectionIn){
+
+    }
 
 }
