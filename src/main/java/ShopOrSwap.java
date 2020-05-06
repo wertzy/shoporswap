da22a27747f2c9ae2b7c1cde89f594b97750ed8f
Justin Moczynski <jmoczynski@ithaca.edu>
2020-05-06 02:37:10 -0400

    updated class structure: ShopOrSwap class; updated automated test implementation: ShopOrSwap class

--- src/main/java/ShopOrSwap.java
+++ src/main/java/ShopOrSwap.java
@@ -24,11 +24,31 @@ public class ShopOrSwap {
     }
 
     /**
+     * Adds an Account to the ShopOrSwap system
+     * @param typeIn
+     * @param nameIn
+     * @param passwordIn
+     * @return
+     */
+    public Account addAccount(String typeIn, String nameIn, String passwordIn){
+        return null;
+    }
+
+    /**
      * Finds an Account in the ShopOrSwap system
      * @param accountIn
      * @return
      */
     public Account findAccount(Account accountIn){
+        return null;
+    }
+
+    /**
+     * Finds an Account in the ShopOrSwap system
+     * @param nameIn
+     * @return
+     */
+    public Account findAccount(String nameIn){
         return null;
     }
 
@@ -42,34 +62,65 @@ public class ShopOrSwap {
     }
 
     /**
-     * Adds a Storefront to the ShopOrSwap system
-     * @param storefrontIn
+     * Removes an Account from the ShopOrSwap system
+     * @param nameIn
      * @return
      */
-    public Storefront addStorefront(Storefront storefrontIn){
+    public Account removeAccount(String nameIn){
+        return null;
+    }
+
+    /**
+     * Adds a Storefront to the ShopOrSwap system
+     * @param nameIn the name of the Storefront to add
+     * @param ownerIn the owner of the Storefront to add
+     * @return
+     * @throws IllegalArgumentException if nameIn is invalid for Storefront name
+     * @throws NoSuchElementException if the ownerIn is not a member of the System
+     * @throws IllegalArgumentException if a Storefront already exists for ownerIn with the name of nameIn
+     */
+    public Storefront addStorefront(String typeIn, String nameIn, Client ownerIn){
         return null;
     }
 
     /**
      * Finds a Storefront in the ShopOrSwap system
-     * @param storefrontIn
+     * @param nameIn the name of the Storefront
+     * @param ownerIn the owner of the Storefront
      * @return
+     * @throws NoSuchElementException if the Storefront cannot be found
      */
-    public Storefront findStorefront(Storefront storefrontIn){
+    public Storefront findStorefront(String nameIn, Client ownerIn){
         return null;
     }
 
     /**
      * Removes a Storefront from the ShopOrSwap system
-     * @param storefrontIn
+     * @param nameIn the name of the Storefront to remove
+     * @param ownerIn the owner of the Storefront to remove
      * @return
+     * @throws NoSuchElementException if the Storefront cannot be found
      */
-    public Storefront removeStorefront(Storefront storefrontIn){
+    public Storefront removeStorefront(String nameIn, Client ownerIn){
         return null;
     }
 
     /**
      * Adds a product to a Storefront
+     * @param nameIn
+     * @param descriptionIn
+     * @param valueIn
+     * @param storefrontIn
+     * @return
+     * @throws IllegalArgumentException if nameIn, descriptionIn, or valueIn are invalid
+     * @throws NoSuchElementException if storefrontIn does not exist (if owner Client of storefrontIn does not own the instance of storefrontIn)
+     */
+    public AbstractProduct addToStorefront(String nameIn, String descriptionIn, double valueIn, Storefront storefrontIn){
+        return null;
+    }
+
+    /**
+     * Adds a product object to a Storefront
      * @param productIn
      * @param storefrontIn
      * @return
@@ -91,6 +142,17 @@ public class ShopOrSwap {
     }
 
     /**
+     * Finds a product in a Storefront
+     * @param nameIn
+     * @param storefrontIn
+     * @return
+     * @throws NoSuchElementException if productIn does not exist in storefrontIn (if owner Client of storefrontIn does not own the instance of productIn)
+     */
+    public AbstractProduct findInStorefront(String nameIn, Storefront storefrontIn){
+        return null;
+    }
+
+    /**
      * Removes a product from the Storefront
      * @param productIn
      * @param storefrontIn
@@ -98,6 +160,17 @@ public class ShopOrSwap {
      * @throws NoSuchElementException if productIn does not exist in storefrontIn (if owner Client of storefrontIn does not own the instance of productIn)
      */
     public AbstractProduct removeFromStorefront(AbstractProduct productIn, Storefront storefrontIn){
+        return null;
+    }
+
+    /**
+     * Removes a product from the Storefront
+     * @param nameIn
+     * @param storefrontIn
+     * @return
+     * @throws NoSuchElementException if productIn does not exist in storefrontIn (if owner Client of storefrontIn does not own the instance of productIn)
+     */
+    public AbstractProduct removeFromStorefront(String nameIn, Storefront storefrontIn){
         return null;
     }
 
--- src/test/java/ShopOrSwapTest.java
+++ src/test/java/ShopOrSwapTest.java
@@ -158,7 +158,22 @@ public class ShopOrSwapTest {
      */
     @Test
     void addStorefrontTest(){
-        assertTrue(false);
+        Client testClient1 = new Client("test1", "pass1");
+
+        ShopOrSwap testShopOrSwap = new ShopOrSwap();
+        testShopOrSwap.addAccount(testClient1);
+
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.addStorefront("sell", "test1", testClient1));
+        Storefront testSellStorefront1 = testShopOrSwap.addStorefront("sell","test1", testClient1);
+        assertEquals("test1", testSellStorefront1.getStorefrontName());
+        assertEquals(1, testClient1.getMyStorefronts());
+        assertEquals(testSellStorefront1, testClient1.getMyStorefronts().get("test1"));
+        assertThrows(IllegalArgumentException.class, ()-> testShopOrSwap.addStorefront("sell", "test1", testClient1));
+        assertThrows(IllegalArgumentException.class, ()-> testShopOrSwap.addStorefront("swap", "test1", testClient1));
+        Storefront testSellStorefront2 = testShopOrSwap.addStorefront("swap","test2", testClient1);
+        assertEquals("test2", testSellStorefront2.getStorefrontName());
+        assertEquals(2, testClient1.getMyStorefronts());
+        assertEquals(testSellStorefront2, testClient1.getMyStorefronts().get("test2"));
     }
 
     /**
@@ -166,7 +181,20 @@ public class ShopOrSwapTest {
      */
     @Test
     void findStorefrontTest(){
-        assertTrue(false);
+        Client testClient1 = new Client("test1", "pass1");
+
+        ShopOrSwap testShopOrSwap = new ShopOrSwap();
+        testShopOrSwap.addAccount(testClient1);
+
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findStorefront("test1", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findStorefront("test2", testClient1));
+
+        Storefront testSellStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+        assertEquals(testSellStorefront1, testShopOrSwap.findStorefront("test1", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findStorefront("test2", testClient1));
+
+        Storefront testSwapStorefront1 = testShopOrSwap.addStorefront("swap", "test2", testClient1);
+        assertEquals(testSwapStorefront1, testShopOrSwap.findStorefront("test2", testClient1));
     }
 
     /**
@@ -174,31 +202,164 @@ public class ShopOrSwapTest {
      */
     @Test
     void removeStorefrontTest(){
-        assertTrue(false);
+        Client testClient1 = new Client("test1", "pass1");
+
+        ShopOrSwap testShopOrSwap = new ShopOrSwap();
+        testShopOrSwap.addAccount(testClient1);
+
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test1", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test2", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test3", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test4", testClient1));
+
+        Storefront testStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+        assertEquals(testStorefront1, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test2", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test3", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test4", testClient1));
+
+        testStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+        Storefront testStorefront2 = testShopOrSwap.addStorefront("sell", "test2", testClient1);
+        assertEquals(testStorefront1, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertEquals(testStorefront2, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test3", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test4", testClient1));
+
+        testStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+        testStorefront2 = testShopOrSwap.addStorefront("sell", "test2", testClient1);
+        Storefront testStorefront3 = testShopOrSwap.addStorefront("swap", "test3", testClient1);
+        assertEquals(testStorefront1, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertEquals(testStorefront2, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertEquals(testStorefront3, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeStorefront("test4", testClient1));
+
+        testStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+        testStorefront2 = testShopOrSwap.addStorefront("sell", "test2", testClient1);
+        testStorefront3 = testShopOrSwap.addStorefront("swap", "test3", testClient1);
+        Storefront testStorefront4 = testShopOrSwap.addStorefront("swap", "test4", testClient1);
+        assertEquals(testStorefront1, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertEquals(testStorefront2, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertEquals(testStorefront3, testShopOrSwap.removeStorefront("test1", testClient1));
+        assertEquals(testStorefront4, testShopOrSwap.removeStorefront("test1", testClient1));
+
     }
 
     /**
-     * Automated tests for ShopOrSwap.addToStorefront method
+     * Automated tests for ShopOrSwap.addToStorefront methods
      */
     @Test
-    void addToStorefrontTest(){
-        assertTrue(false);
+    void addToStorefrontTests(){
+        Client testClient1 = new Client("test1", "pass1");
+
+        ShopOrSwap testShopOrSwap = new ShopOrSwap();
+        testShopOrSwap.addAccount(testClient1);
+
+        Storefront testStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+        Storefront testStorefront2 = testShopOrSwap.addStorefront("sell", "test2", testClient1);
+        Storefront testStorefront3 = testShopOrSwap.addStorefront("swap", "test3", testClient1);
+        Storefront testStorefront4 = testShopOrSwap.addStorefront("swap", "test4", testClient1);
+
+        SellProduct testProduct1 = new SellProduct("test1", "description1", 50, testClient1);
+        SwapProduct testProduct2 = new SwapProduct("test3", "description3", 50, testClient1);
+
+        testShopOrSwap.addToStorefront(testProduct1, testStorefront1);
+        assertEquals(testProduct1, ((SellStorefront) testShopOrSwap.findStorefront("test1", testClient1)).findProduct(testProduct1));
+        testShopOrSwap.addToStorefront(testProduct1, testStorefront2);
+        assertEquals(testProduct1, ((SellStorefront) testShopOrSwap.findStorefront("test2", testClient1)).findProduct(testProduct1));
+        testShopOrSwap.addToStorefront(testProduct2, testStorefront3);
+        assertEquals(testProduct2, ((SwapStorefront) testShopOrSwap.findStorefront("test3", testClient1)).findProduct(testProduct2));
+        testShopOrSwap.addToStorefront(testProduct2, testStorefront4);
+        assertEquals(testProduct2, ((SwapStorefront) testShopOrSwap.findStorefront("test4", testClient1)).findProduct(testProduct2));
+
+        testProduct1 = (SellProduct) testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront1);
+        assertEquals(testProduct1, ((SellStorefront) testShopOrSwap.findStorefront("test1", testClient1)).findProduct(testProduct1));
+
+        testProduct1 = (SellProduct) testShopOrSwap.addToStorefront("test2", "description3", 50, testStorefront2);
+        assertEquals(testProduct1, ((SellStorefront) testShopOrSwap.findStorefront("test2", testClient1)).findProduct(testProduct1));
+
+        testProduct2 = (SwapProduct) testShopOrSwap.addToStorefront("test3", "description3", 50, testStorefront3);
+        assertEquals(testProduct2, ((SwapStorefront) testShopOrSwap.findStorefront("test3", testClient1)).findProduct(testProduct2));
+
+        testProduct2 = (SwapProduct) testShopOrSwap.addToStorefront("test4", "description4", 50, testStorefront4);
+        assertEquals(testProduct2, ((SwapStorefront) testShopOrSwap.findStorefront("test4", testClient1)).findProduct(testProduct2));
     }
 
     /**
      * Automated tests for ShopOrSwap.findInStorefront method
      */
     @Test
-    void findInStorefrontTest(){
-        assertTrue(false);
+    void findInStorefrontTests(){
+        Client testClient1 = new Client("test1", "pass1");
+
+        ShopOrSwap testShopOrSwap = new ShopOrSwap();
+        testShopOrSwap.addAccount(testClient1);
+
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findInStorefront(new SellProduct(), new SellStorefront()));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findInStorefront(new SwapProduct(), new SwapStorefront()));
+
+        Storefront testStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+        Storefront testStorefront2 = testShopOrSwap.addStorefront("swap", "test2", testClient1);
+
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findInStorefront(new SellProduct(), testStorefront1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findInStorefront(new SwapProduct(), testStorefront2));
+
+        AbstractProduct testProduct1 = testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront1);
+        assertEquals(testProduct1, testShopOrSwap.findInStorefront("test1", testStorefront1));
+        assertEquals(testProduct1, testShopOrSwap.findInStorefront(testProduct1, testStorefront1));
+        AbstractProduct testProduct2 = testShopOrSwap.addToStorefront("test2", "description2", 50, testStorefront1);
+        assertEquals(testProduct1, testShopOrSwap.findInStorefront("test1", testStorefront1));
+        assertEquals(testProduct1, testShopOrSwap.findInStorefront(testProduct1, testStorefront1));
+        assertEquals(testProduct2, testShopOrSwap.findInStorefront("test2", testStorefront1));
+        assertEquals(testProduct2, testShopOrSwap.findInStorefront(testProduct2, testStorefront1));
+
+        AbstractProduct testProduct3 = testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront2);
+        assertEquals(testProduct3, testShopOrSwap.findInStorefront("test1", testStorefront1));
+        assertEquals(testProduct3, testShopOrSwap.findInStorefront(testProduct3, testStorefront2));
+        AbstractProduct testProduct4 = testShopOrSwap.addToStorefront("test2", "description2", 50, testStorefront2);
+        assertEquals(testProduct3, testShopOrSwap.findInStorefront("test1", testStorefront2));
+        assertEquals(testProduct3, testShopOrSwap.findInStorefront(testProduct3, testStorefront2));
+        assertEquals(testProduct4, testShopOrSwap.findInStorefront("test2", testStorefront2));
+        assertEquals(testProduct4, testShopOrSwap.findInStorefront(testProduct4, testStorefront2));
+
     }
 
     /**
      * Automated tests for ShopOrSwap.removeFromStorefront method
      */
     @Test
-    void removeFromStorefrontTest(){
-        assertTrue(false);
+    void removeFromStorefrontTests(){
+        Client testClient1 = new Client("test1", "pass1");
+
+        ShopOrSwap testShopOrSwap = new ShopOrSwap();
+        testShopOrSwap.addAccount(testClient1);
+
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeFromStorefront(new SellProduct(), new SellStorefront()));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeFromStorefront(new SwapProduct(), new SwapStorefront()));
+
+        Storefront testStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+        Storefront testStorefront2 = testShopOrSwap.addStorefront("swap", "test2", testClient1);
+
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeFromStorefront(new SellProduct(), testStorefront1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeFromStorefront(new SwapProduct(), testStorefront2));
+
+        AbstractProduct testProduct1 = testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront1);
+        assertEquals(testProduct1, testShopOrSwap.removeFromStorefront("test1", testStorefront1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeFromStorefront("test2", testStorefront1));
+
+        testProduct1 = testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront1);
+        assertEquals(testProduct1, testShopOrSwap.removeFromStorefront(testProduct1, testStorefront1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.removeFromStorefront("test2", testStorefront1));
+
+        testProduct1 = testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront1);
+        AbstractProduct testProduct2 = testShopOrSwap.addToStorefront("test2", "description2", 50, testStorefront1);
+        assertEquals(testProduct1, testShopOrSwap.removeFromStorefront("test1", testStorefront1));
+        assertEquals(testProduct2, testShopOrSwap.removeFromStorefront("test2", testStorefront1));
+
+        testProduct1 = testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront1);
+        testProduct2 = testShopOrSwap.addToStorefront("test2", "description2", 50, testStorefront1);
+        assertEquals(testProduct1, testShopOrSwap.removeFromStorefront(testProduct1, testStorefront1));
+        assertEquals(testProduct2, testShopOrSwap.removeFromStorefront(testProduct2, testStorefront1));
+
     }
 
     /**
@@ -206,7 +367,21 @@ public class ShopOrSwapTest {
      */
     @Test
     void buyProductTest(){
-        assertTrue(false);
+        Client testClient1 = new Client("test1", "pass1");
+
+        ShopOrSwap testShopOrSwap = new ShopOrSwap();
+        testShopOrSwap.addAccount(testClient1);
+
+        Client testClient2 = (Client) testShopOrSwap.addAccount("client", "test2", "pass2");
+
+        Storefront testStorefront1 = testShopOrSwap.addStorefront("sell", "test1", testClient1);
+
+        SellProduct testProduct1 = (SellProduct) testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront1);
+
+        testShopOrSwap.buyProduct(testProduct1, testClient2);
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findInStorefront("test1", testStorefront1));
+        assertEquals(1, ((Client) testShopOrSwap.findAccount("test1")).getMyOwnedProductList().size());
+        assertTrue(((Client) testShopOrSwap.findAccount("test1")).getMyOwnedProductList().contains(testProduct1));
     }
 
     /**
@@ -214,7 +389,26 @@ public class ShopOrSwapTest {
      */
     @Test
     void swapProductsTest(){
-        assertTrue(false);
+        Client testClient1 = new Client("test1", "pass1");
+
+        ShopOrSwap testShopOrSwap = new ShopOrSwap();
+        testShopOrSwap.addAccount(testClient1);
+
+        Client testClient2 = (Client) testShopOrSwap.addAccount("client", "test2", "pass2");
+
+        SwapStorefront testStorefront1 = (SwapStorefront) testShopOrSwap.addStorefront("swap", "test1", testClient1);
+        SwapStorefront testStorefront2 = (SwapStorefront) testShopOrSwap.addStorefront("swap", "test1", testClient2);
+
+        SwapProduct testProduct1 = (SwapProduct) testShopOrSwap.addToStorefront("test1", "description1", 50, testStorefront1);
+        SwapProduct testProduct2 = (SwapProduct) testShopOrSwap.addToStorefront("test2", "description1", 50, testStorefront2);
+
+        testShopOrSwap.swapProducts(testProduct1, testProduct2);
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findInStorefront("test1", testStorefront1));
+        assertThrows(NoSuchElementException.class, ()-> testShopOrSwap.findInStorefront("test2", testStorefront2));
+        assertEquals(1, ((Client) testShopOrSwap.findAccount("test1")).getMyOwnedProductList().size());
+        assertEquals(1, ((Client) testShopOrSwap.findAccount("test2")).getMyOwnedProductList().size());
+        assertTrue(((Client) testShopOrSwap.findAccount("test1")).getMyOwnedProductList().contains(testProduct2));
+        assertTrue(((Client) testShopOrSwap.findAccount("test2")).getMyOwnedProductList().contains(testProduct1));
     }
 
     /**
