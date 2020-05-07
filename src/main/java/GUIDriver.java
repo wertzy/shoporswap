cb62fff994161d57b74fbe2bbdf6e6b7c8142e62
IoanDascalu <idascalu@ithaca.edu>
2020-05-07 05:38:01 -0400

    Gui works, now possible to log in, after logging in you switch scenes, big wow

--- src/main/java/GUIDriver.java
+++ src/main/java/GUIDriver.java
@@ -1,5 +1,3 @@
-package gui;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -11,22 +9,18 @@ import java.io.IOException;
 
 public class GUIDriver extends Application {
 
-    private static final String DATA_FILE = "src/main/resources/systemData.json";
-
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
 
-        Parent root = FXMLLoader.load(getClass().getResource("/sceneBuilder.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/logIn.fxml"));
         primaryStage.setTitle("Shop or Swap");
         primaryStage.setScene(new Scene(root, 700, 700));
         primaryStage.show();
     }
 
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         launch(args);
     }
-
-
 }
 
