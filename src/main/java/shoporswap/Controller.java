d0552e20630066bae6b91c3f6578fdfec4b1e0cc
IoanDascalu <idascalu@ithaca.edu>
2020-05-10 22:03:23 -0400

    Made improvements to the GUI, wrote more methods.

--- src/main/java/shoporswap/Controller.java
+++ src/main/java/shoporswap/Controller.java
@@ -17,6 +17,7 @@ import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import util.JsonUtil;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -28,6 +29,18 @@ public class Controller {
     @FXML private TextField signInAccountName;
     @FXML private PasswordField signInPassword;
     @FXML private Label signInInvalidLabel;
+
+    // home page items
+    @FXML private Button viewStorefrontsClicked;
+    @FXML private Label welcomeLabel;
+    @FXML private Label accountRatingLabel;
+    @FXML private Label errorLabel;
+    @FXML private Label myFundsLabel;
+    @FXML private TextField createStorefrontTxtFld;
+    @FXML private RadioButton rbSell;
+    @FXML private RadioButton rbSwap;
+
+
 
     // client home page items
     @FXML private Label clientHomeTitle;
@@ -61,65 +74,33 @@ public class Controller {
         }
     }
 
-    public void signIn(ActionEvent event) throws IOException{
+    public void signIn(ActionEvent event) throws IOException {
         Account attemptedSignInAccount = this.system.signIn(this.signInAccountName.getText(), this.signInPassword.getText());
-        if(attemptedSignInAccount == null){
+        if (attemptedSignInAccount == null) {
             this.signInInvalidLabel.setVisible(true);
-        }else{
+        } else {
             this.currentUser = attemptedSignInAccount;
-            if(this.currentUser.getClass().getName().contains("Client")){
-                Parent clientHomepage = FXMLLoader.load(getClass().getResource("/clientHome.fxml"));
-                Scene clientHomepageScene = new Scene(clientHomepage, 600, 400);
-                Stage clientHomepageWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                clientHomepageWindow.setTitle("Home: " + this.currentUser.getAccountName());
-                clientHomepageWindow.setScene(clientHomepageScene);
-                clientHomepageWindow.setResizable(false);
+            if (this.currentUser.getClass().getName().contains("Client")) {
+                Parent homepage = FXMLLoader.load(getClass().getResource("/homepage2.fxml"));
+            Scene homepageScene = new Scene(homepage, 600, 400);
+            Stage homepageWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            homepageWindow.setTitle("Home: " + this.currentUser.getAccountName());
+            homepageWindow.setScene(homepageScene);
+            homepageWindow.setResizable(false);
+            homepageWindow.show();
 
-                this.clientHomeTitle = (Label) clientHomepageScene.lookup("#clientHomeTitle");
-                this.clientHomeTitle.setText("Home: " + this.currentUser.getAccountName());
+            this.welcomeLabel = (Label) homepageScene.lookup("#welcomeLabel");
+            this.welcomeLabel.setText("WELCOME: " + this.currentUser.getAccountName());
 
-                this.clientHomeAccountInfo = (Label) clientHomepageScene.lookup("#clientHomeAccountInfo");
-                this.clientHomeAccountInfo.setText(
-                        "\tAccount name: " + this.currentUser.getAccountName() +
-                        "\n\tAccount password: " + this.currentUser.getAccountPassword() +
-                        "\n\tAccount funds: " + ((Client) this.currentUser).getWallet() +
-                        "\n\tAccount rating: " + ((Client) this.currentUser).calculateRating()
-                );
+            this.accountRatingLabel = (Label) homepageScene.lookup("#accountRatingLabel");
+            this.accountRatingLabel.setText("Account rating: " + ((Client) this.currentUser).calculateRating());
 
-                this.clientHomeStorefrontsListView = (ListView) clientHomepageScene.lookup("#clientHomeStorefrontsListView");
-                ObservableList<String> storefrontStrings = this.makeStorefrontObservableList(this.system.findAllStorefronts());
-                this.clientHomeStorefrontsListView.getItems().addAll(storefrontStrings);
-                this.clientHomeStorefrontsHeader = (Label) clientHomepageScene.lookup("#clientHomeStorefrontsHeader");
-                this.clientHomeStorefrontsHeader.setText("All Storefronts: " + storefrontStrings.size());
-
-                this.clientHomeMessagesListView = (ListView) clientHomepageScene.lookup("#clientHomeMessagesListView");
-                ObservableList<String> messageStrings = this.makeMessageObservableList(this.system.findMessagesByRecipient(this.currentUser));
-                this.clientHomeMessagesListView.getItems().addAll(messageStrings);
-                this.clientHomeMessagesHeader = (Label) clientHomepageScene.lookup("#clientHomeMessagesHeader");
-                this.clientHomeMessagesHeader.setText("My Messages: " + messageStrings.size());
-
-
-                this.clientHomeStorefrontsListView.getSelectionModel().selectedItemProperty().addListener(
-                    new ChangeListener<String>(){
-                        @Override
-                        public void changed(ObservableValue<? extends String> observableValue, String old_val, String new_val) {
-                            Controller.this.clientHomeGoToStorefrontNameLabel = (Label) clientHomepageScene.lookup("#clientHomeGoToStorefrontNameLabel");
-                            Controller.this.clientHomeGoToStorefrontOwnerLabel = (Label) clientHomepageScene.lookup("#clientHomeGoToStorefrontOwnerLabel");
-                            String[] recordStringComponents = ((String) clientHomeStorefrontsListView.getSelectionModel().getSelectedItem()).split(":");
-                            String storefrontOwner = recordStringComponents[0].trim();
-                            String storefrontName = recordStringComponents[1].trim();
-                            Controller.this.clientHomeGoToStorefrontNameLabel.setText(storefrontName);
-                            Controller.this.selectedUser = Controller.this.system.findAccount(storefrontOwner);
-                            Controller.this.clientHomeGoToStorefrontOwnerLabel.setText(storefrontOwner);
-                            Controller.this.selectedStorefront = Controller.this.system.findStorefront(storefrontName, (Client) Controller.this.selectedUser);
-                        }
-                    }
-                );
-
-                clientHomepageWindow.show();
-
-            }
+            this.myFundsLabel = (Label) homepageScene.lookup("#myFundsLabel");
+            this.myFundsLabel.setText("Account funds: " + ((Client) this.currentUser).getWallet());
         }
+
+        }
+
     }
 
     public void createAccount(ActionEvent event) throws IOException{
@@ -146,6 +127,83 @@ public class Controller {
         this.signInAccountName.setText("");
         this.signInPassword.setText("");
     }
+    public void viewStorefrontsClicked(ActionEvent event)throws IOException{
+        Parent clientHomepage = FXMLLoader.load(getClass().getResource("/clientHome2.fxml"));
+        Scene clientHomepageScene = new Scene(clientHomepage, 600, 400);
+        Stage clientHomepageWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        clientHomepageWindow.setScene(clientHomepageScene);
+        clientHomepageWindow.setResizable(false);
+
+        this.clientHomeStorefrontsListView = (ListView) clientHomepageScene.lookup("#clientHomeStorefrontsListView");
+        ObservableList<String> storefrontStrings = this.makeStorefrontObservableList(this.system.findAllStorefronts());
+        this.clientHomeStorefrontsListView.getItems().addAll(storefrontStrings);
+        this.clientHomeStorefrontsHeader = (Label) clientHomepageScene.lookup("#clientHomeStorefrontsHeader");
+        this.clientHomeStorefrontsHeader.setText("All Storefronts: " + storefrontStrings.size());
+
+//                this.clientHomeMessagesListView = (ListView) clientHomepageScene.lookup("#clientHomeMessagesListView");
+//                ObservableList<String> messageStrings = this.makeMessageObservableList(this.system.findMessagesByRecipient(this.currentUser));
+//                this.clientHomeMessagesListView.getItems().addAll(messageStrings);
+//                this.clientHomeMessagesHeader = (Label) clientHomepageScene.lookup("#clientHomeMessagesHeader");
+//                this.clientHomeMessagesHeader.setText("My Messages: " + messageStrings.size());
+
+
+        this.clientHomeStorefrontsListView.getSelectionModel().selectedItemProperty().addListener(
+                new ChangeListener<String>(){
+                    @Override
+                    public void changed(ObservableValue<? extends String> observableValue, String old_val, String new_val) {
+                        Controller.this.clientHomeGoToStorefrontNameLabel = (Label) clientHomepageScene.lookup("#clientHomeGoToStorefrontNameLabel");
+                        Controller.this.clientHomeGoToStorefrontOwnerLabel = (Label) clientHomepageScene.lookup("#clientHomeGoToStorefrontOwnerLabel");
+                        String[] recordStringComponents = ((String) clientHomeStorefrontsListView.getSelectionModel().getSelectedItem()).split(":");
+                        String storefrontOwner = recordStringComponents[0].trim();
+                        String storefrontName = recordStringComponents[1].trim();
+                        Controller.this.clientHomeGoToStorefrontNameLabel.setText(storefrontName);
+                        Controller.this.selectedUser = Controller.this.system.findAccount(storefrontOwner);
+                        Controller.this.clientHomeGoToStorefrontOwnerLabel.setText(storefrontOwner);
+                        Controller.this.selectedStorefront = Controller.this.system.findStorefront(storefrontName, (Client) Controller.this.selectedUser);
+                    }
+                }
+        );
+
+        clientHomepageWindow.show();
+
+    }
+    public void createStorefront(ActionEvent event){
+        errorLabel.setVisible(false);
+        String nameIn = createStorefrontTxtFld.getText();
+        String typeIn ="";
+        if(rbSell.isSelected()){
+            typeIn ="Sell";
+        }
+        else if(rbSwap.isSelected()){
+            typeIn ="Swap";
+        }
+        else{
+            errorLabel.setText("Error: Please select a type for the new storefront");
+            errorLabel.setVisible(true);
+        }
+        if(rbSell.isSelected() || rbSwap.isSelected()) {
+            Storefront storefront;
+            try {
+                storefront = system.addStorefront(typeIn, nameIn, (Client) system.findAccount(currentUser));
+                System.out.print("\nStorefront " + storefront.getStorefrontName() + " has been successfully created for products to " + typeIn);
+                System.out.println("We did it");
+            } catch (IllegalArgumentException e) {
+                errorLabel.setText("\nError: " + e.getMessage());
+                errorLabel.setVisible(true);
+            }
+        }
+    }
+
+    public void viewMyStorefrontsClicked(ActionEvent event){}
+
+    public void addFundsClicked(ActionEvent event){}
+
+    public void viewMessagesClicked(ActionEvent event){}
+
+    public void viewMyProductsClicked(ActionEvent event){}
+
+    public void postProductClicked(ActionEvent event){}
+
 
     private ObservableList<String> makeStorefrontObservableList(List<Storefront> storefrontListIn){
         ObservableList<String> observationsOut = FXCollections.<String>observableArrayList();
@@ -168,6 +226,30 @@ public class Controller {
     public void goToStorefront(ActionEvent event){
 
     }
+    public void goToProduct(ActionEvent event){
+
+    }
+    public void back(ActionEvent event)throws IOException{
+
+        Parent homepage = FXMLLoader.load(getClass().getResource("/homepage2.fxml"));
+        Scene homepageScene = new Scene(homepage, 600, 400);
+        Stage homepageWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        homepageWindow.setScene(homepageScene);
+        homepageWindow.setResizable(false);
+        homepageWindow.show();
+
+        //this.current User gives an error, not sure why.
+
+//        this.welcomeLabel = (Label) homepageScene.lookup("#welcomeLabel");
+//        this.welcomeLabel.setText("WELCOME: " + this.currentUser.getAccountName());
+//
+//        this.accountRatingLabel.setText("Account rating: " + ((Client) this.currentUser).calculateRating());
+//
+//        this.myFundsLabel.setText("Account funds: " + ((Client) this.currentUser).getWallet());
+
+
+    }
+
 
     public void signOut(ActionEvent event) throws IOException{
         Parent signInPage = FXMLLoader.load(getClass().getResource("/signIn.fxml"));
